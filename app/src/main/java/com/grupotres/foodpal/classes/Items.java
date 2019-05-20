49d9b0aaee6b7b95a7d2846d16c811ffe274b91e
Florencia <florencia.rodriguez.a@hotmail.com>
2019-05-20 16:29:31 -0300

    Agregando datos reales a la lista

--- app/src/main/java/com/grupotres/foodpal/classes/Items.java
+++ app/src/main/java/com/grupotres/foodpal/classes/Items.java
@@ -6,23 +6,24 @@ public class Items {
     private ArrayList<Item> items = new ArrayList<>();
 
     public Items() {
-        items.add(new Item("Mate", "Test", 1, 1, 1, 1));
-        items.add(new Item("Cafe", "Test", 1, 1, 1, 1));
-        items.add(new Item("Harina", "Test", 1, 1, 1, 1));
-        items.add(new Item("Palmitos", "Test", 1, 1, 1, 1));
-        items.add(new Item("Yerba", "Test", 1, 1, 1, 1));
-        items.add(new Item("Mermelada", "Test", 1, 1, 1, 1));
-        items.add(new Item("Cacao", "Test", 1, 1, 1, 1));
-        items.add(new Item("Picadillo", "Test", 1, 1, 1, 1));
-        items.add(new Item("Pate", "Test", 1, 1, 1, 1));
-        items.add(new Item("Caballa", "Test", 1, 1, 1, 1));
-        items.add(new Item("Arroz", "Test", 1, 1, 1, 1));
-        items.add(new Item("Arvejas", "Test", 1, 1, 1, 1));
-        items.add(new Item("Sardinas", "Test", 1, 1, 1, 1));
-        items.add(new Item("Atun", "Test", 1, 1, 1, 1));
-        items.add(new Item("Choclo", "Test", 1, 1, 1, 1));
-        items.add(new Item("Lentejas", "Test", 1, 1, 1, 1));
+        items.add(new Item("Mate", "infusion", 94, 19, 2, 0));
+        items.add(new Item("Cafe", "infusion", 2, 0, 1, 0));
+        items.add(new Item("Harina", "Cereales y derivados", 455, 95, 13, 1));
+        items.add(new Item("Palmitos", "Fruta", 9, 2, 1, 0));
+        items.add(new Item("Yerba", "Planta", 30, 6, 2, 0));
+        items.add(new Item("Mermelada", "Conserva dulce", 27, 8, 0, 0));
+        items.add(new Item("Cacao", "Planta de Hoja", 12, 3, 1, 1));
+        items.add(new Item("Picadillo", "Relleno", 444, 2, 29, 32));
+        items.add(new Item("Pate", "Pasca cosmetible", 29, 0, 1, 3));
+        items.add(new Item("Caballa", "Pez marino", 182, 0, 19, 12));
+        items.add(new Item("Arroz Integral", "Semilla", 355, 74, 7, 2));
+        items.add(new Item("Arvejas", "Semilla", 42, 6, 5, 1));
+        items.add(new Item("Sardinas", "Pezcado", 224, 0, 23, 15));
+        items.add(new Item("Atun", "Pez", 60, 1, 13, 1));
+        items.add(new Item("Choclo", "Grano", 97,22, 3, 1));
+        items.add(new Item("Lentejas", "Legumbre", 325, 54, 25,2));
     }
+
 
     public ArrayList<Item> getItems() {
         return items;
